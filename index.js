862c54a3338c45ff9da5becc467652af623bbac3
Yinusa <yamusa2@uic.edu>
2020-10-26 00:35:26 -0500

    added dote

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config() // uses enviromental variables so heroku doesnt crash
 const express = require('express')
 
 const server = express()
@@ -5,12 +6,14 @@ const server = express()
 server.use(express.json())
 
 server.use('/', (req,res) =>{
-    res.status(200).json({message:"Hello THE WORLDz"})
+    res.status(200).json({message:"Hello THE WORLD"})
 })
 
-const PORT = 5000
+// const PORT = 5000
+const PORT = process.env.PORT || 5000
 
 server.listen(PORT, () => {
         console.log(`Server is listening on port ${PORT}`)
     
 })
+
