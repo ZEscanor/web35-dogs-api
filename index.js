4b7a1ba80c9e06988f5a89b95f608a1b78304739
Yinusa <yamusa2@uic.edu>
2020-10-26 02:21:49 -0500

    added get

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ let dogs = [{
     breed:"German Shepard",
     imageUrl: 'https://en.wikipedia.org/wiki/German_Shepherd#/media/File:German_Shepherd_-_DSC_0346_(10096362833).jpg'
 }]
-
+//So gets go in front of posts
 server.get('/dogs',(req,res)=>{
     try{
         res.status(200).json(dogs)
@@ -22,6 +22,46 @@ server.get('/dogs',(req,res)=>{
         res.status(500).json({errorMessage: "Cannot get DOGS LOL"})
     }
 })
+
+server.get('/dogs/:id',(req,res)=>{
+    const { id } = req.params
+    const findDogById = dog => {
+        return dog.id == id
+    }
+    const foundDog = dogs.find(findDogById)
+    if(!foundDog){
+        res.status(400).json({errorMessage:"cant find ID"})
+    }
+    else{
+        res.json(foundDog)
+    }
+})
+
+//add a new dog
+// same with axios posting this is the server side which it expects when you are posting
+server.post('/dogs',(req,res)=> {
+    const newDog = req.body
+    if(!(newDog.breed)){
+        res.status(400).json({errorMessage:"Please make sure you have a breed"})
+    }try{
+        const notNew = dogs.find(dog=> dog.breed === req.body.breed)
+        if(!notNew){
+            newDog.id = uuid.v4()
+            dogs.push(newDog)
+            res.status(201).json({errorMessage: "Added a new Dog",newDog})
+        }
+        else{
+            res.status(400).json({errorMessage: "404 Dog already found"})
+        }
+    }
+    catch{
+        res.status(500).json({errorMessage:"Make Server plz"})
+    }
+
+})
+
+
+//Important to call the before your use or else your app wont read /dogs
 
 server.use('/', (req,res) =>{
     res.status(200).json({message:"Hello THE WORLD"})
